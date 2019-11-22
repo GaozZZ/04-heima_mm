e4aec4ca7369fa2199a350d1db493c32ed21bd27
GaozZZ <864261199@qq.com>
2019-11-22 15:08:05 +0800

    首页基础建立

--- src/router/router.js
+++ src/router/router.js
@@ -2,8 +2,11 @@ import VueRouter from 'vue-router'
 import Vue from 'vue'
 Vue.use(VueRouter)
 import login from '../view/login/login.vue'
+import index from '../view/index/index.vue'
 const routes = [
-    { path: '/login', component: login }
+    { path: '/login', component: login },
+    { path: '/', redirect: '/login' },
+    { path: '/index', component: index },
 ];
 const router = new VueRouter({
     routes
